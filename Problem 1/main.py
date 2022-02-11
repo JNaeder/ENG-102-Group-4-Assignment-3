b24ecaefd3773ba9c1dd4498d3b22a70e00780fa
JNaeder <J.naeder324@gmail.com>
2022-02-11 12:15:43 -0500

    Added test cases to Problem 1

--- Problem 1/main.py
+++ Problem 1/main.py
@@ -61,6 +61,31 @@ greatest_z_score = z_score(greatest(population))
 # 3. the least value in the population list.
 least_z_score = z_score(least(population))
 
-print("Mean Z-Score = "+ str(mean_z_score))
-print("Greates Z-Score = "+ str(greatest_z_score))
-print("Least Z-Score = "+ str(least_z_score))
+# print("Mean Z-Score = "+ str(mean_z_score))
+# print("Greates Z-Score = "+ str(greatest_z_score))
+# print("Least Z-Score = "+ str(least_z_score))
+
+# TEST CASES
+
+print("\n----------Test 1----------")
+test_data_1 = [2,4]
+print("Expected Mean Output = 0 : Actual Mean Output = " + str(z_score(mean(test_data_1))))
+print("Expected Greatest Output = 1.0 : Actual Greatest Output = " + str(z_score(greatest(test_data_1))))
+print("Expected Least Output = -1.0 : Actual Least Output = " + str(z_score(least(test_data_1))))
+print("\n----------Test 2----------")
+test_data_2 = [6, 7, 7, 12, 13, 13, 15, 16, 19, 22]
+print("Expected Mean Output = 0 : Actual Mean Output = " + str(z_score(mean(test_data_2))))
+print("Expected Greatest Output = 1.79 : Actual Greatest Output = " + str(z_score(greatest(test_data_2))))
+print("Expected Least Output = -1.39 : Actual Least Output = " + str(z_score(least(test_data_2))))
+print("\n----------Test 3----------")
+test_data_3 = [5, 6, 7, 7, 8]
+print("Expected Mean Output = 0 : Actual Mean Output = " + str(z_score(mean(test_data_3))))
+print("Expected Greatest Output = 1.37 : Actual Greatest Output = " + str(z_score(greatest(test_data_3))))
+print("Expected Least Output = -1.57 : Actual Least Output = " + str(z_score(least(test_data_3))))
+print("\n----------Test 4----------")
+test_data_4 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print("Expected Mean Output = 0 : Actual Mean Output = " + str(z_score(mean(test_data_4))))
+print("Expected Greatest Output = 1.65 : Actual Greatest Output = " + str(z_score(greatest(test_data_4))))
+print("Expected Least Output = -1.65 : Actual Least Output = " + str(z_score(least(test_data_4))))
+print("")
+
